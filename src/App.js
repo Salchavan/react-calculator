05eb51707468357ac90afeb9bc2e7774670f5588
Salvador Castellanos <salvadorcastellanosposse@gmail.com>
2025-03-27 09:51:54 -0300

    Implement calculator functionality with input handling and result evaluation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import './css/App.css';
 import Calculator from './components/Calculator.js';
+import { useState } from 'react';
+
+const mathSymbols = ['',
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', // Números
+  '+', '-', '*', '/', // Operadores básicos
+  '.', '(', ')'       // Otros símbolos comunes
+];
 
 function App() {
+  const [calculatorResult, setCalculatorResult] = useState(0);
+  const [calculatorInput, setCalculatorInput] = useState('');
+
+  const clearInput = () => {
+    setCalculatorInput('');
+    setCalculatorResult(0);
+  };
+
+  const handleCalculatorResult = (input) => {
+    if (input.length < 2) {
+      setCalculatorResult('...');
+      return;
+    }
+    try {
+      // Evalúa la expresión de manera segura
+      const result = eval(input);
+      if (isNaN(result) || typeof result !== 'number') {
+        throw new Error('Expresión inválida');
+      }
+      setCalculatorResult(result);
+    } catch (error) {
+      console.error('Error al calcular la expresión:', error.message);
+      setCalculatorResult('Error');
+    }
+  };
+
+  const handlePressButton = (text) => {
+    const input = calculatorInput + text;
+    setCalculatorInput(input);
+    handleCalculatorResult(input);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Calculator</h1>
       </header>
-      <Calculator className='calculator'/>
+      <Calculator 
+        className='calculator'
+        calculatorResult={calculatorResult}
+        setCalculatorResult={setCalculatorResult}
+        calculatorInput={calculatorInput}
+        setCalculatorInput={setCalculatorInput}
+        clearInput={clearInput}
+        mathSymbols={mathSymbols}
+        handleCalculatorResult={handleCalculatorResult}
+        handlePressButton={handlePressButton}/>
     </div>
   );
 };
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,35 +1,54 @@
 import React from "react";
 import '../css/Calculator.css';
 
-function Calculator() {
+function Calculator({calculatorResult, handleCalculatorResult, calculatorInput, setCalculatorInput, clearInput, mathSymbols, handlePressButton}) {
+
   return (
     <div className="calculator-container">
       <div className='div-screen'>
-        <span className="screen-input">1 + 1</span>
+        <input 
+          type="text" 
+          className="screen-input" 
+          onChange={(event) => {
+            const input = event.target.value;
+            const lastChar = input.slice(-1); 
+            if (mathSymbols.includes(lastChar)) {
+              setCalculatorInput(input);
+              console.log(input);
+              handleCalculatorResult(input); 
+            } else {
+              console.log(lastChar + ' - No es un símbolo permitido');
+            }
+          }}
+          value={calculatorInput}
+        />
         <div className="screen-output">
           <span className="screen-equal">= </span>
-          <span className="screen-result">2</span>
+          <span className="screen-result">{calculatorResult}</span>
         </div>
-        
       </div>
       <div className='div-buttons'>
-        <button className="btn-number btn-0">0</button>
-        <button className="btn-number btn-1">1</button>
-        <button className="btn-number btn-2">2</button>
-        <button className="btn-number btn-3">3</button>
-        <button className="btn-number btn-4">4</button>
-        <button className="btn-number btn-5">5</button>
-        <button className="btn-number btn-6">6</button>
-        <button className="btn-number btn-7">7</button>
-        <button className="btn-number btn-8">8</button>
-        <button className="btn-number btn-9">9</button>
-        <button className="btn-operator btn-add"><i className="fa-solid fa-lg fa-plus"></i></button>
-        <button className="btn-operator btn-subtract"><i className="fa-solid fa-lg fa-minus"></i></button>
-        <button className="btn-operator btn-multiply"><i class="fa-solid fa-lg fa-xmark"></i></button>
-        <button className="btn-operator btn-divide"><i class="fa-solid fa-lg fa-divide"></i></button>
-        <button className="btn-operator btn-equal"><i class="fa-solid fa-lg fa-equals"></i></button>
-        <button className="btn-operator btn-decimal">0.</button>
-        <button className="btn-operator btn-clear">C</button>
+        <button className="btn-number btn-0" onClick={(e) => {
+          handlePressButton(e.target.textContent);
+        }}>0</button>
+        <button className="btn-number btn-1" onClick={(e) => handlePressButton(e.target.textContent)}>1</button>
+        <button className="btn-number btn-2" onClick={(e) => handlePressButton(e.target.textContent)}>2</button>
+        <button className="btn-number btn-3" onClick={(e) => handlePressButton(e.target.textContent)}>3</button>
+        <button className="btn-number btn-4" onClick={(e) => handlePressButton(e.target.textContent)}>4</button>
+        <button className="btn-number btn-5" onClick={(e) => handlePressButton(e.target.textContent)}>5</button>
+        <button className="btn-number btn-6" onClick={(e) => handlePressButton(e.target.textContent)}>6</button>
+        <button className="btn-number btn-7" onClick={(e) => handlePressButton(e.target.textContent)}>7</button>
+        <button className="btn-number btn-8" onClick={(e) => handlePressButton(e.target.textContent)}>8</button>
+        <button className="btn-number btn-9" onClick={(e) => handlePressButton(e.target.textContent)}>9</button>
+        <button className="btn-operator btn-add" onClick={(e) => handlePressButton('+')}><i className="fa-solid fa-lg fa-plus"></i></button>
+        <button className="btn-operator btn-subtract" onClick={(e) => handlePressButton('-')}><i className="fa-solid fa-lg fa-minus"></i></button>
+        <button className="btn-operator btn-multiply" onClick={(e) => handlePressButton('*')}><i className="fa-solid fa-lg fa-xmark"></i></button>
+        <button className="btn-operator btn-divide" onClick={(e) => handlePressButton('/')}><i className="fa-solid fa-lg fa-divide"></i></button>
+        <button className="btn-operator btn-equal" onClick={(e) => handlePressButton('')}><i className="fa-solid fa-lg fa-equals"></i></button>
+        <button className="btn-operator btn-decimal" onClick={(e) => handlePressButton('.')}>0.</button>
+        <button className="btn-operator btn-clear" 
+          onClick={clearInput}
+        >C</button>
       </div>
     </div>
   );
